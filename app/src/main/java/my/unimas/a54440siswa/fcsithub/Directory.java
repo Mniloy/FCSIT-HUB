db6cc801a85e06db3abc022c50ef471c5bc42199
Mniloy <54440@siswa.unimas.my>
2018-08-06 01:55:33 +0800

    logout working

---
+++ app/src/main/java/my/unimas/a54440siswa/fcsithub/Directory.java
@@ -0,0 +1,13 @@
+package my.unimas.a54440siswa.fcsithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Directory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directory);
+    }
+}
--- app/src/main/java/my/unimas/a54440siswa/fcsithub/HomeActivity.java
+++ app/src/main/java/my/unimas/a54440siswa/fcsithub/HomeActivity.java
@@ -1,32 +1,83 @@
 package my.unimas.a54440siswa.fcsithub;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button BTNLogout;
-    private FirebaseAuth mAuth;
+    Button BTNFacilities, BTNDirectory;
+    ImageView IVLogout;
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         mAuth = FirebaseAuth.getInstance();
+
+        BTNFacilities = (Button) findViewById(R.id.BTNfacilities);
+        BTNDirectory = (Button) findViewById(R.id.BTNdirectory);
+        IVLogout = (ImageView) findViewById(R.id.IVLogout);
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    startActivity(new Intent(HomeActivity.this, SignIn.class));
+                }
+            }
+        };
+
+        IVLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+
+            }
+        });
+
+        BTNFacilities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent facilities = new Intent (HomeActivity.this, Facilities.class);
+                startActivity(facilities);
+            }
+        });
+
+        BTNDirectory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent facilities = new Intent (HomeActivity.this, Directory.class);
+                startActivity(facilities);
+            }
+        });
+
 
     }
 }
